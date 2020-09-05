9c01f1ed038fe1ec9d2eedd3736560cdd37f5a1a
Maksym Kravchuk <kravchukmaxym@gmail.com>
2020-09-05 20:28:20 +0300

    Added 'slider'

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,28 @@
 let $ = require('jquery')
 let slick = require('./slick.js')
+
+$(document).ready(function () {
+	const body = $('body')
+	const slider = $('.slider__list')
+	const menuToggle = $('.js-menu-toggle')
+	const nav = $('.nav')
+	const main = $('.main')
+
+	menuToggle.on('click', function () {
+		body.toggleClass('overflow')
+		main.toggleClass('blur')
+		$(this).toggleClass('is-active')
+		nav.toggleClass('is-active')
+	})
+
+	if (slider.length) {
+		slider.slick({
+			infinite: true,
+			dots: true,
+			appendArrows: $('.slider__controls'),
+			appendDots: $('.slider__controls'),
+			autoplay: true,
+			autoplaySpeed: 5000
+		});
+	}
+});
